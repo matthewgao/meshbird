8675073c46961333abff455826df17cd8f3da111
gaoshen.gs <gaoshen.gs@alibaba-inc.com>
2018-10-01 01:22:09 +0800

    update

--- transport/client.go
+++ transport/client.go
@@ -40,6 +40,7 @@ func (c *Client) Start() {
 		c.conns[connIndex] = conn
 
 		go c.conns[connIndex].run()
+		time.Sleep(time.Second) // FIXME:
 		go c.conns[connIndex].runRead()
 	}
 	c.mutex.Unlock()
